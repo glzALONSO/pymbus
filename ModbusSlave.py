934d1515e805dff6d2604da3e022ce4d1735d8e0
goamx <glzalonso55@gmail.com>
2024-09-25 18:46:22 -0600

    A modbus slave class was created

---
+++ ModbusSlave.py
@@ -0,0 +1,97 @@
+from pymodbus.client import ModbusSerialClient
+from pymodbus import ExceptionResponse, FramerType, ModbusException, pymodbus_apply_logging_config
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+
+
+class ModbusSlave:
+    # define the instance attributes
+    def __init__(self, client, dev_addr, byteorder, wordorder):
+        # User defined instance attributes
+        self.client = client
+        self.dev_addr = dev_addr
+        self.byteorder = byteorder
+        self.wordorder = wordorder
+        # Storage
+        self.registers = []
+        self.decoder = None
+        self.decoded_payload = []
+        self.measurements = {}
+
+    def set_up_decoder(self):
+        if self.byteorder == "big":
+            byte_endian = Endian.BIG
+
+        elif self.byteorder == "little":
+            byte_endian = Endian.LITTLE
+        
+        else:
+            print("Failed to set up decoder")
+            return
+                
+        if self.wordorder == "big":
+            word_endian = Endian.BIG
+
+        elif self.wordorder== "little":
+            word_endian = Endian.LITTLE
+
+        else:
+            print("Failed to set up decoder")
+            return
+        if self.registers == []:
+            print("No payload to decode")
+            self.decoder = None
+        else:
+            self.decoder = BinaryPayloadDecoder.fromRegisters(self.registers, byteorder=byte_endian, wordorder=word_endian)
+        return self.decoder
+    
+
+    def poll_device(self, start_addr, regs_count):
+        try:
+            response = self.client.read_holding_registers(address=start_addr, count=regs_count, slave=self.dev_addr)
+
+        except ModbusException as exc:
+            print(f"Received ModbusException({exc}) from library")
+            self.registers = []
+            raise(exc)
+        
+        if response.isError():
+            print(f"Received Modbus library error({response})")
+            self.registers = []
+
+        elif isinstance(response, ExceptionResponse):
+            print(f"Received Modbus library exception ({response})")
+            self.registers = []
+            # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
+
+        else:
+            self.registers = response.registers
+            
+        return self.registers
+
+
+    def decode_registers(self, decoding_function):
+        if self.decoder == None:
+            self.decoded_payload = []
+            return self.decoded_payload
+        else:
+            try:
+                self.decoded_payload = decoding_function(self.decoder)
+            except:
+                print(f"All register from device with address: {self.dev_addr} decoded.")
+                
+            return self.decoded_payload
+    
+    def get_measurements(self, start_addr, regs_count, decoding_function):
+        self.measurements = {}
+        try:
+            self.poll_device(start_addr, regs_count)
+            self.set_up_decoder()
+            self.decode_registers(decoding_function)
+            self.measurements[self.dev_addr] = self.measurements.get(self.dev_addr, self.decoded_payload)
+
+        except Exception:
+            print("failed to get measurements")
+            raise Exception
+        
+        return self.measurements
